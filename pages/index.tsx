aab33a0faa8a5d4f42f7402ba984810f9505a839
Suhail Kakar <56798748+suhailkakar@users.noreply.github.com>
2023-03-31 05:10:26 +0530

    setup next & tailwind app

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="font-extrabold text-transparent h-40 text-8xl bg-clip-text bg-gradient-to-r from-orange-400 to-yellow-400">
+        gm!
+      </h1>
+    </div>
+  );
+}
